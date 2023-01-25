c047a4a2a3a30a5c33cbf04b90f225fbda336807
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-25 12:21:57 -0800

    refactored context to get rid of passCard function in app.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
   const { playerOneHand, 
     playerTwoHand, 
     playerThreeHand, 
-    setPlayerOneHand, 
-    setPlayerTwoHand, 
-    setPlayerThreeHand } = useContext(GameContext);
+    passCard } = useContext(GameContext);
 
   const [deck, setDeck] = useState(initialCards);
   const [selectedCard, setSelectedCard] = useState();
@@ -21,28 +19,6 @@ function App() {
 
   function findCardIndex(value, suit, cards) {
     return cards.findIndex((card) => card.value === value && card.suit === suit);
-  }
-
-  function passCard(card) {
-    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
-    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
-
-    // arrays start at zero, but our players start at 1 :shrug:
-    const toHand = playerHands[to - 1] || deck;
-    const fromHand = playerHands[from - 1] || deck;
-
-    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
-    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
-
-    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
-    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
-
-    toHand.push(cardToMove);
-
-    toSetFunction([...toHand]);
-    fromSetFunction([...fromHand]);
-
-    setSelectedCard(null);
   }
 
   return (
