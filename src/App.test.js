ccddf9a6d3e328383fd2a88d7856b3c10bbde12e
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-25 15:38:24 -0800

    wrapped app render in game context provider in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { GameProvider } from './Context/GameContext.js';
 
-test.skip('renders different players', () => {
-  render(<App />);
+test('renders different players', () => {
+  render(
+    <GameProvider>
+      <App />
+    </GameProvider>
+  );
   const player1 = screen.getByText(/Player 1/i);
   const player2 = screen.getByText(/Player 2/i);
   const player3 = screen.getByText(/Player 3/i);
